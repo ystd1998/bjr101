e8c1227f9f8ebcd9dc5ceff52769c5dd791d4406
ystd1998 <ystd1998@gmail.com>
2018-03-18 22:28:33 -0700

    .

---
+++ setup/u16/06_install_pdftex.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-base texlive-latex-recommended \
+  texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra \
+  texlive-latex-base texlive-font-utils
+
