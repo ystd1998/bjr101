b3005552ed7059323dfb43dd20cb1ce9bc3b91a8
ystd1998 <ystd1998@gmail.com>
2018-04-29 03:58:53 +0000

    .

---
+++ setup/u16/07_install_r_pdf.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# http://rprogramming.net/create-html-or-pdf-files-with-r-knitr-miktex-and-pandoc/
+
+# Step 1: Install MiKTeX
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+sudo apt-get update
+
+sudo apt-get install -y miktex pandoc
+
+sudo apt-get install texlive-latex-base -y
